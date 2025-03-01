1c739180b3bd61f6ca525e6e4a675fe806c84d1f
Jerry <x.jerry.wang@gmail.com>
2025-03-01 18:30:22 +0800

    chore: fix release config

--- release.conf.ts
+++ release.conf.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from '@0x-jerry/x-release'
 
 export default defineConfig({
+  publish: false,
   async beforeCommit(ctx) {
     await ctx.run('npm run changelog')
-  }
+  },
+  tasks: [
+    //
+    (ctx) => ctx.run('npm run publish'),
+  ],
 })
